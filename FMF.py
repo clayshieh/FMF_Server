f2dcb0a4972f980985b3daedec2c35648871595a
Clay Shieh <shieh.clay@gmail.com>
2018-02-13 10:27:42 -0800

    changed function names and updated logic

--- FMF.py
+++ FMF.py
@@ -9,6 +9,8 @@ import uuid
 import json
 import time
 import requests
+import logging
+
 
 class FMFException(Exception):
 	def __init__(self, value):
@@ -17,8 +19,9 @@ class FMFException(Exception):
 	def __str__(self):
 		return repr(self.value)
 
+
 class FMF():
-	def __init__(self, aid, password):
+	def __init__(self, aid, password, cache=True, verbose=True):
 		# credentials
 		self.aid = aid
 		self.password = password
@@ -37,16 +40,27 @@ class FMF():
 		self.first_run = True
 
 		# cached info
-		self.path = os.path.dirname(os.path.abspath(__file__))
-		self.cpath = os.path.join(self.path, "contacts.json")
-		self.fpath = os.path.join(self.path, "fmf.json")
-		if os.path.isfile(self.cpath):
-			self.contacts = self.persistant_read(self.cpath)
-		if os.path.isfile(self.fpath):
-			self.fmf_map = self.persistant_read(self.fpath)
+		self.cache = cache
+		if self.cache:
+			self.path = os.path.dirname(os.path.abspath(__file__))
+			self.cpath = os.path.join(self.path, "contacts.json")
+			self.fpath = os.path.join(self.path, "fmf.json")
+			if os.path.isfile(self.cpath):
+				self.contacts = self.persistant_read(self.cpath)
+			if os.path.isfile(self.fpath):
+				self.fmf_map = self.persistant_read(self.fpath)
+
+		# Logger
+		logging.basicConfig()
+		self.logger = logging.getLogger(__name__)
+		self.logger.setLevel(logging.DEBUG)
+		if verbose:
+			self.logger.setLevel(logging.INFO)
+
+		self.authenticate()
 
 	def persistant_write(self, fname, data):
-		#data is a dictionary
+		# data is a dictionary
 		with open(fname, 'w') as f:
 			json.dump(data, f)
 
@@ -71,25 +85,27 @@ class FMF():
 		max_tries = 3
 		exp_time = 0.0625
 
-		#bad code practice. If LAN is down then it will hit the exception case
-		#if apple server is down then itll get stuck in while loop and try with exponential backoff
+		# bad code practice. If LAN is down then it will hit the exception case
+		# if apple server is down then itll get stuck in while loop and try with exponential backoff
 		while not r:
-			#just in case
+			# just in case
 			if count > max_tries:
-				print "Max tries reached"
+				self.logger.info("Max tries reached")
 				return None
 
 			try:
 				r = functions[method](url, headers=headers, json=body, cookies=self.cookies)
 			except Exception as e:
-				print "Error in request"
-				print e
+				self.logger.debug("Error in request")
+				self.logger.debug("Error: " + str(e))
+				self.logger.debug("Response: " + str(r))
+
 				r = None
 				time.sleep(wait_time)
 				continue
 
-			#exponential back off
-			if exp_time <= 16384: #lowest freq is ~ once per hr for apple server to come up
+			# exponential back off
+			if exp_time <= 16384: # lowest freq is ~ once per hr for apple server to come up
 				exp_time *= 2
 				count = 0
 			count += 1
@@ -97,22 +113,24 @@ class FMF():
 
 		return r
 
-
 	def get_service_url(self, resp, service):
 		if resp:
 			if service in resp["webservices"].keys():
 				if resp["webservices"][service]["status"] == "active":
+					self.logger.info("FMF service enabled on account")
 					self.fmf_base_url = resp["webservices"][service]["url"]
 					return
 		raise FMFException("Please check that FMF is enabled on your iCloud account.")
 
 	def get_dsid(self, resp):
 		if resp:
+			self.logger.info("Login succesful")
 			self.dsid = resp["dsInfo"]["dsid"]
 		else:
 			raise FMFException("Please check that your login information is correct.")
 
 	def authenticate(self):
+		self.logger.info("Authenticating FMF service")
 		auth_url = "https://setup.icloud.com/setup/ws/1/login?clientBuildNumber={0}&clientId={1}"
 		auth_url = auth_url.format(self.build_id, self.client_id)
 
@@ -136,6 +154,7 @@ class FMF():
 		self.get_service_url(auth_resp, "fmf")
 
 	def refresh(self, init=False):
+		self.logger.info("Refresh called")
 		action = "refresh"
 		if init:
 			action = "init"
@@ -143,43 +162,40 @@ class FMF():
 		fmf_url = "{0}/fmipservice/client/fmfWeb/{1}Client?clientBuildNumber={2}&clientId={3}&dsid={4}"
 
 		headers = {
-			"Origin":"https://www.icloud.com",
-			"Referer":"https://www.icloud.com"
+			"Origin": "https://www.icloud.com",
+			"Referer": "https://www.icloud.com"
 		}
 
 		data = {
-			"clientContext":{
-				"productType":"fmfWeb",
-				"appVersion":"1.0",
-				"contextApp":"com.icloud.web.fmf",
-				"userInactivityTimeInMS":1,
-				"tileServer":"Apple"
+			"clientContext": {
+				"productType": "fmfWeb",
+				"appVersion": "1.0",
+				"contextApp": "com.icloud.web.fmf",
+				"userInactivityTimeInMS": 1,
+				"tileServer": "Apple"
 			}
 		}
 
 		fmf_url = fmf_url.format(self.fmf_base_url, action, self.build_id, self.client_id, self.dsid)
 
-		r = self.request(fmf_url, "POST", 
-						headers=headers,
-						body=data
-					)
+		r = self.request(fmf_url, "POST", headers=headers, body=data)
 
-		#update the cookies
+		# update the cookies
 		self.update_cookies(r)
 
-		#process data
+		# process data
 		data = r.json()
 
-		#get contacts
+		# get contacts
 		name2id = {}
 		if "contactDetails" in data:
 			for contact in data["contactDetails"]:
 				name = contact["firstName"] + " " + contact["lastName"]
 				name2id[name] = contact["id"]
 
-		#get locations
-		#k: id
-		#v: [timestamp(ms), country, streetname, streetaddress, coutnrycode, locality, statecode, administrativearea]
+		# get locations
+		# k: id
+		# v: [timestamp(ms), country, streetname, streetaddress, coutnrycode, locality, statecode, administrativearea]
 		fmf_map = {}
 		if "locations" in data:
 			for location in data["locations"]:
@@ -188,61 +204,99 @@ class FMF():
 					address = location["location"]["address"]
 					if address:
 						fmf_map[location["id"]] = [timestamp, address]
-					continue #sometimes address isn't ready yet
+					continue # sometimes address isn't ready yet
 
 		return name2id, fmf_map
 
-	def find(self, tries=7, min_tries=2, wait_time=3):
+	def update(self, tries=7, min_tries=2, wait_time=3):
+		self.logger.info("Find called")
 		if self.first_run:
-			#run init first
+			# run init first
 			self.contacts, self.fmf_map = self.refresh(init=True)
 
 		for i in range(tries):
 			new_contacts, new_fmf_map = self.refresh()
 
-			#update if anything changed in contacts
+			# update if anything changed in contacts
 			if new_contacts != self.contacts:
-				print "contacts are different"
-				print "old contacts"
-				print self.contacts
+				self.logger.info("Contacts are different")
+				self.logger.info("Old contacts:")
+				self.logger.info(self.contacts)
 				self.contacts.update(new_contacts)
-				print "updated contacts"
-				print self.contacts
+				self.logger.info("Updated contacts:")
+				self.logger.info(self.contacts)
 
-
+			self.logger.info("Updating fmf_map")
 			different = False
-			#check if anything changed in fmf_map
+			# check if anything changed in fmf_map
 			for f in new_fmf_map:
-				#checks if anything changed
+				# checks if anything changed
 				if f in self.fmf_map:
 					if new_fmf_map[f][1] != self.fmf_map[f][1]:
 						self.fmf_map[f] = new_fmf_map[f]
 						different = True
-				#new_fmf_map has something new
+				# new_fmf_map has something new
 				else:
 					self.fmf_map[f] = new_fmf_map[f]
 					different = True
 
-			#nothing changed and not the first run
+			# nothing changed and not the first run
 			if not different and i >= min_tries:
-				print "nothing changed"
+				self.logger.info("nothing changed")
 				break
 
 			time.sleep(wait_time)
 
-		self.persistant_write(self.cpath, self.contacts)
-		self.persistant_write(self.fpath, self.fmf_map)
+		# error handling
+		if not self.contacts:
+			self.logger.debug("Contacts is empty")
+			return None
+		if not self.fmf_map:
+			self.logger.debug("FMF map is empty")
+			return None
 
+		if self.cache:
+			self.persistant_write(self.cpath, self.contacts)
+			self.persistant_write(self.fpath, self.fmf_map)
 
-	def get_user(self, user, hook=None):
-		#use hooks as functions to run other utilities with that information
+	def get_user_by_name(self, user, update=False, hook=None):
+		self.logger.info("Finding user: {0}".format(user))
+		if update:
+			# update data
+			self.update()
+
+		# use hooks as functions to run other utilities with that information
 		if user in self.contacts:
 			if self.contacts[user] in self.fmf_map:
 				result = self.fmf_map[self.contacts[user]]
-				# print result
 				if hook:
 					hook(user, result)
 				return result
-		print "User {0} not in contacts".format(self.contacts[user])
-		self.refresh()
+				
+		self.logger.debug("User {0} not in contacts or can't be found right now".format(self.contacts[user]))
+		return None
+
+	def get_user_by_id(self, uid, update=False, reverse=True, hook=None):
+		self.logger.info("Finding user id: {0}".format(uid))
+		if update:
+			# update data
+			self.update()
+
+		# find the user name associated with the user id
+		user = None
+		for u, u_id in self.contacts.iteritems():
+			if u_id == uid:
+				if reverse:
+					user = u
+				else:
+					user = u_id
+
+		if user:
+			if uid in self.fmf_map:
+				result = self.fmf_map[uid]
+				if hook:
+					hook(user, result)
+				return result
+				
+		self.logger.debug("UserID {0} not in contacts or can't be found right now".format(uid))
 		return None
--- README.md
+++ README.md
@@ -15,26 +15,27 @@ Import FMF into your Python file by including the following line
 ### Usage
 Initialize the client with your apple ID login which should be your email and password.
 
-`f = FMF(aid="fmf@example.com", password="password")`
+`f = FMF("fmf@example.com", "password")`
 
-Call the `authenticate()` method
+When you want to refresh the location status of all your Find My Friend contacts, call the `update()` method
 
-`f.authenticate()`
-
-When you want to refresh the location status of your Find My Friend contacts, call the `find()` method
-
-`f.find()`
+`f.update()`
 
 You can also specify the number of minimum attempts (to get a more accurate reading), the maximum number of attempts (to prevent significant delays) and the wait time (in seconds) between each attempt to avoid spamming Apple servers. The default settings that I've personally found to be good enough are as follows: 2 minimum attempts, 7 maximum attempts and a 3 second wait time.
 
-When you want to get the location data of a contact use the `get_user()` method with the contact's **contact name as it appears in your contacts**.
+When you want to get the location data of a contact use the `get_user_by_name()` method with the contact's **contact name as it appears in your contacts**.
 
-`f.get_user("Best Friend")`
+`f.get_user_by_name("Best Friend")`
+
+the api currently does not play well with contacts with emoji's in the name so an alternative method to find the contact is provided, `get_user_by_id()`.
+
+`f.get_user_by_id("~QLKSDFKL8.DK")`
 
 You can also specify a function with the optional `hook` argument to do something with the user's name and the returned data. For example if you are using a SMS Api that has a `text()` function, if you set `hook=text` then it will execute `text(user, result)` where user is the contact name of the person you are trying to find and result is the data of said user's current location information.
 
 ### Notes
   * Whenever the script authenticates the iCloud login, the script will trigger Apple to send you an email that a device has logged into your iCloud. I believe Apple has implemented a cookie/session variable system to remember devices that log into iCloud. Currently this API does not have the functionality to remember said cookie, store it, and reuse the cookie whenever authenticate is called so if you call authenticate n times you will receive n emails from Apple. Apple does not require you to take any actions to confirm the identity of the device so it does not affect the functionality of the API. 
+  * This API does **NOT** work when 2FA is enabled on the icloud account.
 
 ### Acknowledgements
 I initially referenced Vladimir Smirnov's iCloud API for general iCloud workflow and cookie implementation which can be found here
